39c2f0d9fc4a61a76de594d93957ea56df85d172
Szymon Botor <botorszym@gmail.com>
2021-05-16 15:43:57 +0200

    Troika! initiative stack mostly finished

--- troika.py
+++ troika.py
@@ -1,38 +1,83 @@
+from random import choices
+
 class InitiativeStack:
     
+    END = 'The End of the Round'
+    ENEM = 'Enemies'
+    HENCH = 'Henchmen'
+    
     def put_back(self):
-        self.stack = []
-        for key in self.tokens.keys():
-            self.stack.extend([self.tokens[key]] * key)
+        for value in self._tokens.values():
+            value[0] = value[1]
     
     def reset(self):
-        self.tokens: dict = {
-            'End of the Round': 1,
-            'Enemies': 0,
-            'Henchmen': 0
+        self._tokens: dict = {
+            self.END: [1, 1],
+            self.ENEM: [0, 0],
+            self.HENCH: [0, 0]
         }
         self.put_back()
     
     def __init__(self):
         # Variable declarations
-        self.tokens: dict = {}
-        self.stack: list = []
+        self._tokens: dict = {}
         
         self.reset()
         self.put_back()
     
     def clear(self):
-        self.tokens['Enemies'] = 0
+        # Clear the enemy and henchmen tokens
+        self._tokens[self.ENEM] = [0, 0]
+        self._tokens[self.HENCH] = [0, 0]
+        self.put_back()
 
     def add_player(self, name):
-        self.tokens[name] = 2
+        if name not in (self.END, self.ENEM, self.HENCH):
+            self._tokens[name] = [2, 2]
+        else:
+            raise ValueError('Invalid character name')
 
     def rmv_player(self, name):
-        # Delete the key (catch the KeyError somewhere higher if raised)
-        del self.tokens[name]
+        if name not in (self.END, self.ENEM, self.HENCH):
+            try:
+                del self._tokens[name]
+            except KeyError:
+                raise ValueError('No such character exists')
+        else:
+            raise ValueError('Invalid character name')
 
-    def remove_player(self, name):
-        return self.tokens.pop(name, None)
+    def __iadd__(self, other):
+        self._tokens[self.ENEM][0] += other
+        self._tokens[self.ENEM][1] += other
+        return self
 
+    def draw(self):
+        weights = []
+        for value in self._tokens.values():
+            weights.append(value[0])
+
+        drawn = choices(list(self._tokens.keys()), weights=weights, k=1)[0]
+        if drawn == 'The End of the Round':
+            self.put_back()
+        else:
+            self._tokens[drawn][0] -= 1
+
+        return drawn
+
+    def add_players(self, *args):
+        for name in args:
+            self.add_player(name)
+
+# Testing
 st = InitiativeStack()
-print(st.tokens)
+st += 12
+st.add_players('P1', 'P2', 'P3')
+st.put_back()
+
+while True:
+    #print(st.stack)
+    drawn = st.draw()
+    print(drawn, st._tokens)
+    
+    if drawn == st.END:
+        break
